fc78dd7424b9605bce56c96b54da4a49e4c4a733
Gaizka Alonso <gaizkalonso1@gmail.com>
2025-02-17 12:53:38 +0100

    Tonto quien lo lea

--- web_codeurv/src/components/Footer.jsx
+++ web_codeurv/src/components/Footer.jsx
@@ -5,7 +5,55 @@ function Footer() {
       <footer className="footer">
         <div>
           <img src ="../../public/la_calle.jpg" alt="Logo"/>
-          <p>Together we code the future</p>
+        </div>
+
+        <div id="footer-section">
+          <h1>Community</h1>
+          <ul>
+            <li>
+              Whatsapp
+            </li>
+            <li>
+              Instagram
+            </li>
+            <li>
+              LinkedIn
+            </li>
+          </ul>
+        </div>
+
+        <div id="footer-section">
+          <h1>Projects</h1>
+          <ul>
+            <li>
+              Catle
+            </li>
+            <li>
+              Snake IA
+            </li>
+            <li>
+              Web
+            </li>
+          </ul>
+        </div>
+
+        <div id="footer-section">
+          <h1>Pages</h1>
+          <ul>
+            <li>
+              Inicio
+            </li>
+            <li>
+              Eventos
+            </li>
+            <li>
+              Equipos
+            </li>
+            <li>
+              Socios
+            </li>
+          </ul>
+
         </div>
   
       </footer>
