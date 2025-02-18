31b736531de786c849b808b7a582d9f723374b2e
“Ivan <ivan.carayol@estudiants.urv.cat>
2025-02-18 09:54:23 +0100

    creacion de apartados faltantes

---
+++ web_codeurv/src/components/SobreNosotros.jsx
@@ -0,0 +1,11 @@
+import "../styles/SobreNosotros.css"
+
+function SobreNosotros() {
+  return (
+    <div className="SobreNosotros">
+      <p>Sobre nosotros</p>
+    </div>
+  );
+}
+
+export default SobreNosotros;
