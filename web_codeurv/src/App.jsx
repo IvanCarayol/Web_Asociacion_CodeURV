0254f68743fc2a5a0979955477f36cd5b7eb3ebd
Gabriel <gabriel.ruizprieto@gmail.com>
2025-02-18 16:09:18 +0100

    merge

--- web_codeurv/src/App.jsx
+++ web_codeurv/src/App.jsx
@@ -4,11 +4,12 @@ import Inicio from "./components/Inicio";
 import Footer from "./components/Footer";
 import Equipos from "./components/Equipos";
 import SobreNosotros from "./components/SobreNosotros";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <NavBar /> <Inicio /> <Equipos /> <SobreNosotros /> <Footer />
+      <NavBar /> <Inicio /> <Equipos /> <SobreNosotros /> <Form /> <Footer />
     </>
   );
 }
