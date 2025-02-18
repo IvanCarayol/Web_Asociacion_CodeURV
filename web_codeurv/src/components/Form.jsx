3ef8189c8e50909978dc01f5d6dc41e1f0f9a621
Gabriel <gabriel.ruizprieto@gmail.com>
2025-02-18 17:14:17 +0100

    FORM casi acabado: bastante CSS y el HTML

--- web_codeurv/src/components/Form.jsx
+++ web_codeurv/src/components/Form.jsx
@@ -7,36 +7,44 @@ function Form() {
         <h2 class="form__title">¡CONTACTA CON NOSOTROS!</h2>
         <fieldset class="contacto__info-general">
             <legend>Información general</legend>
-              <div class="form__input-label__container">
+              <div class="form__input-label__container name">
                 <input id="Name" name="Name" class="form__input" type="text" placeholder="Juan" request></input>
                 <label for="Name">Tu nombre:</label>
               </div>
-              <div class="form__input-label__container">
-                <input id="Apellido" name="Apellido" class="form__input" type="text" placeholder="Pablo"></input>
-                <label for="Apellido">Tus Apellidos:</label>
+              <div class="form__input-label__container surname">
+                <input id="Surname" name="Surname" class="form__input" type="text" placeholder="Pablo"></input>
+                <label for="Surname">Tus Apellidos:</label>
               </div>
-              <div class="form__input-label__container">
+              <div class="form__input-label__container email">
                 <input id="Email" name="Email" class="form__input" type="email" placeholder="ejemplo@correo.com" request></input>
                 <label for="Email">Tu correo:</label>
               </div>
-              <div class="form__input-label__container">
+              <div class="form__input-label__container tel">
                 <input id="Tel" name="Tel" class="form__input" type="tel" placeholder="1111111111"></input>
                 <label for="Tel">Tu teléfono:</label>
               </div>
         </fieldset>
         <fieldset class="contacto__info-msg"> 
           <legend>Déjanos un mensaje:</legend>
-          <div class="form__input-label__container">
-            <p class="form__text">¿Estudias Ingeniería Informática?</p>
-            <input type="radio" id="si" name="estudia" value="si"></input>
-            <label for="si">Sí</label>
-            <input type="radio" id="no" name="estudia" value="no"></input>
-            <label for="no">No</label>
+          <div class="form__input-label__container msg">
+            <div class="question__container">
+              <p class="form__text">¿Estudias Ingeniería Informática?</p>
+              <div class="form__input-yes">
+                <input type="radio" id="si" name="estudia" value="si"></input>
+                <label for="si">Sí</label>
+              </div>
+              <div class="form__input-no">
+                <input type="radio" id="no" name="estudia" value="no"></input>
+                <label for="no">No</label>
+              </div>
+            </div>
           </div> 
-          <p class="form__text">Mensaje:</p>
-          <textarea id="mensaje" name="mensaje" placeholder="Tu mensaje"></textarea>
+          <div class="form__msg-container">
+            <p class="form__text">Mensaje:</p>
+            <textarea id="mensaje" name="mensaje" placeholder="Tu mensaje"></textarea>
+          </div>
         </fieldset>
-        <input id="Enviar" name="Enviar" type="submit" value="Enviar"></input>
+        <input id="Enviar" name="Enviar" class="enviar" type="submit" value="Enviar" ></input>
       </form>
     </section>
     
